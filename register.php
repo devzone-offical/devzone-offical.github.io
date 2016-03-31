2f415b3fe2ee9e1abb7d1366cc81480641c307ee
Hari krishnan <rajahari1996@gmail.com>
2016-03-31 23:32:27 +0530

    bug fix #1 - register

--- register.php
+++ register.php
@@ -3,8 +3,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'u281253775_reg');
 define('DB_USER','u281253775_user');
 define('DB_PASSWORD','muneer@123.');
-$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+$con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Failed to connect to MySQL: " . mysqli_connect_errno());
 
 function NewUser() {
 	$fullname = $_POST['name'];
@@ -13,7 +12,7 @@ function NewUser() {
 	$contact = $_POST['contact'];
 	$stream = $_POST['stream'];
 	$query = "INSERT INTO users (fullname,email,gender,contact,stream) VALUES ('$fullname','$email','$gender','$contact','$stream')";
-	$data = mysql_query ($query) or die(mysql_error());
+	$data = mysqli_query ($con,$query) or die(mysqli_connect_errno());
 	if($data) {
 		echo "YOUR REGISTRATION IS COMPLETED...";
 	}
@@ -22,8 +21,8 @@ function NewUser() {
 function SignUp() {
 	if(!empty($_POST['email'])) //checking
 	{
-		$query = mysql_query("SELECT * FROM users WHERE email = '$_POST[email]'") or die(mysql_error());
-		if(!$row = mysql_fetch_array($query) or die(mysql_error())){
+		$query = mysqli_query($con,"SELECT * FROM users WHERE email = '$_POST[email]'") or die(mysqli_connect_errno());
+		if(!$row = mysqli_fetch_array($query) or die(mysqli_connect_errno())){
 			NewUser();
 		}
 		else {
